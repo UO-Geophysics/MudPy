c9349ef4eab2ed0f25b33e23fd57af0af0f935ea
dmelgarm <trucutu.dm@gmail.com>
2014-06-29 12:14:23 -0700

    Updates

--- run/template.fwd.py
+++ run/template.fwd.py
@@ -13,6 +13,7 @@ from obspy.core import UTCDateTime
 
 home='/Volumes/Kanagawa/Slip_Inv/'
 project_name='tohoku_tsunami'
+run_name='dv'
 ################################################################################
 
 
@@ -20,26 +21,28 @@ project_name='tohoku_tsunami'
 
 init=0 #Initalize project
 make_green=0 #Compute GFs
-make_synthetics=1 #Compute synthetics for a given model at given stations
-solve=0  # =1 solves forward problem or runs inverse calculation, =0 does nothing
+make_synthetics=0 #Compute synthetics for a given model at given stations
+solve=1  # =1 solves forward problem or runs inverse calculation, =0 does nothing
 ###############################################################################
 
-##############ipython#            Green function parameters               #############
+###############            Green function parameters               #############
 coord_type=1 #(=0 for cartesian, =1 for lat/lon (will use Earth flattening transform)
-hot_start=118  #Start at a certain subfault number
+hot_start=75  #Start at a certain subfault number
 static=0  #=1 computes static GFs only, =0 computes the complete waveform
+tsunami=1
 model_name='fnet_cmt.mod'   #Velocity model
-rupture_name='tohoku_tsunami.rupt'   #Rupture model, not needed for inversion
+rupture_name='tohoku_dv.rupt'   #Rupture model, not needed for inversion
 fault_name='tohoku_tsunami.fault'    #Fault geometry
-station_file='tsunami.sta'    #Station distribution
-NFFT=256 ; dt=2  #Time parameters
+station_file='tsunami.sta'   #Station distribution
+NFFT=128 ; dt=4  #Time parameters
 dk=0.2 ; pmin=0 ; pmax=1 ; kmax=10   #fk integration parameters
 ################################################################################
 
 ############                 Synthetics parameters               ###############
 time_epi=UTCDateTime('2011-03-11T05:46:23')
+resample=2 #Resample synthetics to this rate (in Hz)
 integrate=1 #=0 produces velocities, =1 makes displacements
-beta=0
+beta=0 #Rake offset, usually a good idea to keep at zero
 ################################################################################
 
 
@@ -49,20 +52,21 @@ if init==1:
 
 # Run green functions          
 if make_green==1: 
-    runslip.make_green(home,project_name,station_file,fault_name,model_name,dt,NFFT,static,hot_start,coord_type,dk,pmin,pmax,kmax)  
+    runslip.make_green(home,project_name,station_file,fault_name,model_name,dt,NFFT,static,
+                tsunami,beta,hot_start,coord_type,dk,pmin,pmax,kmax)  
 
 #Now make synthetics for source/station pairs
 if make_synthetics==1:
     #runslip.rupt2fault(home,project_name,rupture_name) 
-    runslip.make_synthetics(home,project_name,station_file,fault_name,model_name,integrate,static,beta,
-                    hot_start,coord_type,time_epi)
+    runslip.make_synthetics(home,project_name,station_file,fault_name,model_name,integrate,
+                static,tsunami,beta,hot_start,coord_type,time_epi)
     
 #Run forward comptuation or solve for inverse problem
 if solve==1:
     if static==0: #Forward problem (full waveforms)
-        forward.waveforms(home,project_name,rupture_name,station_file,model_name,integrate,hot_start,resample)
+        forward.waveforms(home,project_name,rupture_name,station_file,model_name,run_name,integrate,tsunami,hot_start,resample)
     if static==1: #Forward problem (coseismics)
-        forward.coseismics(home,project_name,rupture_name,station_file)
+        forward.coseismics(home,project_name,rupture_name,run_name,station_file)
     
     
     
--- run/template.inv.py
+++ run/template.inv.py
@@ -9,11 +9,10 @@ Comment: Inversion of Tohoku data with 10s rise times, epicenter location from C
 from mudpy import runslip
 import numpy as np
 from obspy.core import UTCDateTime
-
 ########                            GLOBALS                             ########
-home='/Users/dmelgarm/Research/Slip_Inv/'
+home='/Volumes/Kanagawa/Slip_Inv/'
 project_name='tohoku_10s'
-run_name='20win_dv_final_0.5Hz'
+run_name='20win_42_fine2'
 ################################################################################
 
 
@@ -23,19 +22,22 @@ init=0 #Initalize project
 make_green=0 #Compute GFs
 make_synthetics=0 #Compute synthetics for a given model at given stations
 G_from_file=1 # =0 read GFs and create a new G, =1 load G from file
-invert=0  # =1 runs inversion, =0 does nothing
+invert=1  # =1 runs inversion, =0 does nothing
 ###############################################################################
 
 ###############            Green function parameters               #############
 coord_type=1 # =0 for cartesian, =1 for lat/lon (will use Earth flattening transform)
-hot_start=0   #Start at a certain subfault number
+hot_start=0  #Start at a certain subfault number
 model_name='fnet_cmt.mod'   #Velocity model
 fault_name='tohoku.fault'    #Fault geometry
 station_file='tohoku.sta'    #Station distribution
-GF_list='tohoku.disp.vel.gflist' #What GFs are to be computed for each station
-G_name='fnet_20win_vr4_200s_0.5Hz_velmult36.g' #Either name of GF matrix to load or name to save GF matrix with
+GF_list='tohoku.dvt.gflist'#What GFs are to be computed for each station
+tgf_file='tohoku.tgf'
+G_name='fnet_20win_vr3.5_300s_dvt_mul1disp_mul40vel_mul1.1_OB2tsun.g.npy' #Either name of GF matrix to load or name to save GF matrix with
 # Displacement and velocity waveform parameters
 NFFT=2048 ; dt=0.25
+#Tsunami deformation parameters
+tsunNFFT=128 ; tsun_dt=4.0
 #fk-parameters
 dk=0.1 ; pmin=0 ; pmax=1 ; kmax=20
 ################################################################################
@@ -43,10 +45,10 @@ dk=0.1 ; pmin=0 ; pmax=1 ; kmax=20
 #############               Inversion Parameters               ################# 
 epicenter=np.array([142.68,38.19,21.0])    #lon,lat,depth(positive in km)
 time_epi=UTCDateTime('2011-03-11T05:46:23')
-rupture_speed=4.0 #Fastest rupture allowed in km/s
+rupture_speed=3.5 #Fastest rupture allowed in km/s
 num_windows=20
-reg_spatial=np.logspace(-0.5,0.5,num=4) #Set to False if you don't want to use it
-reg_temporal=np.logspace(-1.0,0.0,num=4) #Set to False if don't want to use it
+reg_spatial=np.logspace(0.25,0.35,num=2) #Set to False if you don't want to use it
+reg_temporal=np.logspace(0,0,num=1) #Set to False if don't want to use it
 nstrike=21 ; ndip=9 ; nfaults=(nstrike,ndip)
 beta=45 #Rotational offset (in degrees) applied to rake (0 for normal)
 solver='nnls' # 'lstsq','nnls'
@@ -55,8 +57,8 @@ bounds=(top,bottom,left,right)
 ################################################################################
 
 ########      Run-time modifications to the time series             ############
-decimate=2   #Decimate by constant (=None for NO decimation)
-lowpass=0.5 #Low pass corner frequency in Hz =None if no filter is desired
+decimate=4  #Decimate by constant (=None for NO decimation)
+lowpass=None #Low pass corner frequency in Hz =None if no filter is desired
 ################################################################################
 
 #Initalize project folders
@@ -65,8 +67,9 @@ if init==1:
     
 # Run green functions          
 if make_green==1 or make_synthetics==1:
-    runslip.inversionGFs(home,project_name,GF_list,fault_name,model_name,dt,NFFT,
-         coord_type,make_green,make_synthetics,dk,pmin,pmax,kmax,beta,time_epi,hot_start)   
+    runslip.inversionGFs(home,project_name,GF_list,tgf_file,fault_name,model_name,
+        dt,tsun_dt,NFFT,tsunNFFT,coord_type,make_green,make_synthetics,dk,pmin,
+        pmax,kmax,beta,time_epi,hot_start)   
 
 #Run inversion
 if invert==1:
