Added fix for contour problem in distances calculation

src/python/mudpy/fakequakes.py
@@ -114,7 +114,7 @@ def subfault_distances_3D(home,project_name,fault_name,slab_name,projection_zone
 
     """
 
-    from numpy import sqrt,sin,cos,deg2rad,zeros,meshgrid,linspace,where,c_,unravel_index,sort,diff,genfromtxt,sign,unique
+    from numpy import sqrt,sin,cos,deg2rad,zeros,meshgrid,linspace,where,c_,unravel_index,sort,diff,genfromtxt,sign,argmin
     from scipy.interpolate import griddata
     from matplotlib import pyplot as plt
     from scipy.spatial.distance import cdist
@@ -239,6 +239,23 @@ def subfault_distances_3D(home,project_name,fault_name,slab_name,projection_zone
         x_range=slab_x.max()-slab_x.min()
         x_down_dip=linspace(-x_range/2,x_range/2,200)
         
+        #get contours
+        all_contours=[]
+        contour_lengths=zeros(len(fault))
+        print('Calculcating slab contours')
+        for i in range(len(fault)):
+            if i%10==0:
+                print('... working on subfault '+str(i)+' of '+str(len(fault)))
+            
+            contour=plt.contour(X,Y,Z,levels=fault[i,3])
+            contour=contour.collections[0].get_paths()[0].vertices
+            contour_lengths[i]=((contour[0,0]-contour[-1,0])**2+(contour[0,1]-contour[-1,1])**2)**0.5
+            all_contours.append(contour)
+        
+        #if a contour is shorter than this number use the next deepest
+        minimum_contour_length=0.95*contour_lengths.max()
+        
+        
         #Loop over number of subfaults, we want the distance from i-th fault to all other (j) subfaults
         print('Getting inter-fault distances')
         for i in range(len(fault)):
@@ -249,11 +266,23 @@ def subfault_distances_3D(home,project_name,fault_name,slab_name,projection_zone
             yi = fault_y[i]
             zi = fault_z[i]
             
-            # X, Y and Z are matrices with the grid info, now create one contour at each subfault centroid depth
-            contour=plt.contour(X,Y,Z,levels=fault[i,3])
+            #find most approriate contour for lenght calculation
+            icontour=where(contour_lengths>minimum_contour_length)[0]
             
-            #Get contour at depth of current subfault
-            contour=contour.collections[0].get_paths()[0].vertices
+            #find closest depth_contour of the ones that pass the minimum length
+            deltaZ=abs(fault_z[i]-fault_z)
+            icontour_depth=argmin(deltaZ[icontour])
+            
+            #This si the contour that is long enough and closest in depth
+            icontour_correct=icontour[icontour_depth]
+            contour=all_contours[icontour_correct]
+            
+            
+#            # X, Y and Z are matrices with the grid info, now create one contour at each subfault centroid depth
+#            contour=plt.contour(X,Y,Z,levels=fault[i,3])
+#            
+#            #Get contour at depth of current subfault
+#            contour=contour.collections[0].get_paths()[0].vertices
             
             # Now find coordinates of point on this contour closest to subfault centroid
             dist=sqrt((xi-contour[:,0])**2+(yi-contour[:,1])**2)